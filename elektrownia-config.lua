aea759895cfb2eb554015dc76c1ebeb5a01df13c
mrLeonTheMaster <78276856+mrLeonTheMaster@users.noreply.github.com>
2025-02-09 20:31:56 +0100

    Fixed config

--- elektrownia-config.lua
+++ elektrownia-config.lua
@@ -1,3 +1,8 @@
-engineControllerId = 0
-energyGeneratorControllerId = 0
-fuelFactoryControllerId = 0
+-- ID's of redrouter blocks, if the name of the redrouter block is 'redrouter_5', the id will be 5
+engineControllerId = 0 -- The block controlling the steam engine
+energyGeneratorControllerId = 0 -- The block controlling the power generators
+fuelFactoryControllerId = 0 -- The block controlling the fuel factory
+
+-- End of config, don't edit beyond this point.
+
+return { engineControllerId = engineControllerId, energyGeneratorControllerId = energyGeneratorControllerId, fuelFactoryControllerId = fuelFactoryControllerId }
